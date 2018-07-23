6a35204028aa25cce7ff3317afdd5023a8b903b6
Steve Chae <schae614@gmail.com>
2018-07-22 23:22:26 -0400

    Some answers to section ii

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -5,17 +5,17 @@ fun example1(list: List<Int>) {
     // If a lambda has exactly one parameter, that parameter can be accessed as 'it'
     val positiveNumbers = list.filter { it > 0 }
 
-    val squares = list.map { it * it }
+val squares = list.map { it * it }
 }
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    todoCollectionTask()
+    return this.customers.map { it.city }.toSet()
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    todoCollectionTask()
+    return this.customers.filter { it.city.name == city.name }
 }
 
 
